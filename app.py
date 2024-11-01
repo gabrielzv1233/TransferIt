af12a155ca47a3c14f53aa1b6323ce395cc5dbd3
gabrielzv1233 <26gvuricaga@crschools.us>
2024-11-01 14:34:55 -0500

    allow_unsafe_werkzeug=True

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def handle_file_upload(data):
     }, room=link_uuid)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
